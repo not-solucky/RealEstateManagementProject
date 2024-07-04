68c366f4a91be67e77894678435b1b9e671c2738
NotSoLucky <88020849+not-solucky@users.noreply.github.com>
2024-07-04 21:17:57 +0600

    Update types.go

--- Backend/types/types.go
+++ Backend/types/types.go
@@ -20,15 +20,15 @@ type User struct {
 	Name      string    `json:"username"`
 	Email     string    `json:"email"`
 	Password  string    `json:"-"`
-	Phone	  string    `json:"phone_number"`
+	Phone     string    `json:"phone_number"`
 	ImagePath string    `json:"image"`
-	Role	  string    `json:"role"`
+	Role      string    `json:"role"`
 	Verified  bool      `json:"is_verified"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 type RegisterUserPayload struct {
-	Email    string `json:"email" validate:"required,email"` 
+	Email    string `json:"email" validate:"required,email"`
 	Name     string `json:"username" validate:"required"`
 	Password string `json:"password" validate:"required,min=6,max=100"`
 	Phone    string `json:"phone" validate:"required"`
@@ -39,7 +39,10 @@ type LoginUserPayload struct {
 	Password string `json:"password" validate:"required"`
 }
 
-
+// Define Prperty type
+type Property struct {
+	ID int `json:"id"`
+}
 
 // Define Image type
 type Image struct {
